3590b29e2ab8d85d5a056b6aaf2ca0a88a390c3b
Alex A. Mihaylov <minimumlaw@rambler.ru>
2025-02-02 20:32:08 +0300

    Apply pathes for split sdcards to u-boot setup and working for write to eMMC

--- board/stp/bananapi-m4zero/before_image.sh
+++ board/stp/bananapi-m4zero/before_image.sh
@@ -6,6 +6,7 @@ echo "###########################"
 
 BOARD_DIR="$(dirname $0)"
 
-install -m 0644 -D $BOARD_DIR/extlinux.conf $BINARIES_DIR/extlinux/extlinux.conf
+install -m 0644 -D $BOARD_DIR/extlinux.conf   $BINARIES_DIR/extlinux/extlinux.conf
+install -m 0644 -D $BOARD_DIR/uboot-setup.img $BINARIES_DIR/boot.scr
 
 exit 0
